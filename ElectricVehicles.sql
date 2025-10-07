a2a347b3681f4ff28eeb8a33d90759098d0c9628
SebasLee98 <sebastianhclee@gmail.com>
2025-10-06 17:06:44 -0700

    more tests

--- ElectricVehicles.sql
+++ ElectricVehicles.sql
@@ -3,6 +3,6 @@ SELECT * FROM electric_vehicle_population_data;
 
 SELECT * FROM electric_vehicle_population_data WHERE County = "King" AND City = "Kent";
 
--- awef a rffa aewf daafwaefa
+-- awef a rffa aewf daafwaefaawrfercfsearc
 
 
