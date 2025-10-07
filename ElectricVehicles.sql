374b23313c8b720a22980e9536ab3adae18ac360
SebasLee98 <sebastianhclee@gmail.com>
2025-10-06 17:05:48 -0700

    awercq

---
+++ ElectricVehicles.sql
@@ -0,0 +1,8 @@
+-- Commit 1 test
+SELECT * FROM electric_vehicle_population_data;
+
+SELECT * FROM electric_vehicle_population_data WHERE County = "King" AND City = "Kent";
+
+-- awef a rffa aewf daafwaefa
+
+
