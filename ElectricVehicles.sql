be5a5f6716f6b5a69e07646bf0b33c79fcca0a56
SebasLee98 <sebastianhclee@gmail.com>
2025-10-06 20:56:20 -0700

    Add readme

--- ElectricVehicles.sql
+++ ElectricVehicles.sql
@@ -3,6 +3,8 @@ SELECT * FROM electric_vehicle_population_data;
 
 SELECT * FROM electric_vehicle_population_data WHERE County = "King" AND City = "Kent";
 
+SELECT * FROM electric_vehicle_population_data WHERE Model = "Niro"
+
 -- Tests
 
 
