09523b31ca5096564a80102f7e2c7655f75f175c
SebasLee98 <sebastianhclee@gmail.com>
2025-10-11 20:01:30 -0700

    add excel file

--- ElectricVehicles.sql
+++ ElectricVehicles.sql
@@ -2,4 +2,12 @@
 WITH Seattle AS (
     SELECT * FROM electric_vehicle_population_data WHERE City = "Seattle"
 )
-SELECT  `Legislative District`, count(`VIN (1-10)`) FROM Seattle GROUP BY `Legislative District`
+SELECT  `Legislative District`, count(`VIN (1-10)`) FROM Seattle GROUP BY `Legislative District`;
+
+-- Number of vehicles within each CAFV
+SELECT `Clean Alternative Fuel Vehicle (CAFV) Eligibility` as CAFV, count(DISTINCT `VIN (1-10)`) FROM electric_vehicle_population_data 
+GROUP BY CAFV;
+
+
+
+SELECT * FROM electric_vehicle_population_data
