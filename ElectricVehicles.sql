8b88b3d965f44c4f6f30522d9af711165d584cac
SebasLee98 <sebastianhclee@gmail.com>
2025-10-11 19:45:29 -0700

    newpractice

--- ElectricVehicles.sql
+++ ElectricVehicles.sql
@@ -1,4 +1,5 @@
 -- Most popular legislative district of Seattle
-SELECT * FROM electric_vehicle_population_data WHERE City = "Seattle"
-
-SELECT  `Legislative District`, count(`VIN (1-10)`) FROM electric_vehicle_population_data GROUP BY `Legislative District`
+WITH Seattle AS (
+    SELECT * FROM electric_vehicle_population_data WHERE City = "Seattle"
+)
+SELECT  `Legislative District`, count(`VIN (1-10)`) FROM Seattle GROUP BY `Legislative District`
